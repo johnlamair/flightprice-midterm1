2af93e5db07ceceb0656178884db00f03a20a241
John <156053292+johnlamair@users.noreply.github.com>
2025-10-15 12:45:23 +0000

    updated objectives paragraph

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ st.title("✈️ Flight Price Prediction")
 
 st.markdown("### Objectives")
 st.write(
-    "Our goal is to predict the price of flights based off of multiple variables."
+    "The objective of this analysis is to develop a predictive model that estimates flight prices based on a range of influencing variables. " \
+    "We aim to identify the relationships that most significantly affect ticket pricing."
 )
 
 st.markdown("### Dataset")
