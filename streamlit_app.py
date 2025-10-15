93205b3112769db49a62dcfd467a11eb5f2551f0
John <156053292+johnlamair@users.noreply.github.com>
2025-10-15 13:21:04 +0000

    updated pages, introduction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,41 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.image("images.jpg", use_container_width=True)
+st.sidebar.title("Pages:")
+page = st.sidebar.selectbox("Select Page",["Introduction 📘","Visualization 📊", "Automated Report 📑","Prediction"])
+
 
 st.title("✈️ Flight Price Prediction")
-st.link_button("Github Repo", "https://github.com/johnlamair/flightprice-midterm1")
-
-st.markdown("### 🎯 Objectives")
-st.write(
-    "The objective of this analysis is to develop a predictive model that estimates flight prices based on a range of influencing variables. " \
-    "We aim to identify the relationships that most significantly affect ticket pricing."
-)
-
-st.markdown("### 🎯 Dataset")
-st.write(
-    "Dataset contains information about flight booking options from the website Easemytrip for flight travel between India's top 6 metro cities. There are 300261 datapoints and 11 features in the cleaned dataset."
-)
-
 df = pd.read_csv("flight-price.csv")
-st.dataframe(df.head())
 
-st.write(
-    "Source: https://www.kaggle.com/datasets/shubhambathwal/flight-price-prediction?resource=download"
-)
+
+if page == "Introduction 📘":
+    st.link_button("Github Repo", "https://github.com/johnlamair/flightprice-midterm1")
+    st.image("images.jpg", use_container_width=True)
+
+    st.markdown("### 🎯 Objectives")
+    st.write(
+        "The objective of this analysis is to develop a predictive model that estimates flight prices based on a range of influencing variables. " \
+        "We aim to identify the relationships that most significantly affect ticket pricing."
+    )
+
+    st.markdown("### 🎯 Dataset")
+    st.write(
+        "Dataset contains information about flight booking options from the website Easemytrip for flight travel between India's top 6 metro cities. There are 300261 datapoints and 11 features in the cleaned dataset."
+    )
+
+    df.columns = ["Serial Number", "Airline", "Flight", "Source City", "Departure Time", "Stops", "Arrival Time", "Destination City", "Class", "Duration", "Days Left", "Price"]
+    st.dataframe(df.head())
+
+    st.write(
+        "Source: https://www.kaggle.com/datasets/shubhambathwal/flight-price-prediction?resource=download"
+    )
+
+elif page == "Visualization 📊":
+
+    st.subheader("Data Visualization")
+
+
+    
