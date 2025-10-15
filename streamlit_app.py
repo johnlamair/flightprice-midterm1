ca944005a7acc0d1bcb50c0938cbeeb7ebc98a8a
John <156053292+johnlamair@users.noreply.github.com>
2025-10-15 12:56:36 +0000

    updated titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ import pandas as pd
 st.title("✈️ Flight Price Prediction")
 st.link_button("Github Repo", "https://github.com/johnlamair/flightprice-midterm1")
 
-st.markdown("### Objectives")
+st.markdown("### 🎯 Objectives")
 st.write(
     "The objective of this analysis is to develop a predictive model that estimates flight prices based on a range of influencing variables. " \
     "We aim to identify the relationships that most significantly affect ticket pricing."
 )
 
-st.markdown("### Dataset")
+st.markdown("### 🎯 Dataset")
 st.write(
     "Dataset contains information about flight booking options from the website Easemytrip for flight travel between India's top 6 metro cities. There are 300261 datapoints and 11 features in the cleaned dataset."
 )
